6252f36c2c5e0ed2e4f07de5e154a2947d097dc8
sundownspecial <je.mendoza@live.com>
2018-02-05 16:36:05 -0800

    valid post tests are working

--- lab-jesus/lib/basic-auth-middleware.js
+++ lab-jesus/lib/basic-auth-middleware.js
@@ -1,23 +1,23 @@
-'use strict'
+'use strict';
 
-const errorHandler = require('./error-handler')
+const errorHandler = require('./error-handler');
 
 module.exports = function(req, res, next) {
-  let authHeaders = req.headers.authorization
+  let authHeaders = req.headers.authorization;
   if(!authHeaders)
-    return errorHandler(new Error('Authorization failed. Headers do not match requirements.'), res)
+    return errorHandler(new Error('Authorization failed. Headers do not match requirements.'), res);
 
-  let base64 = authHeaders.split('Basic ')[1]
+  let base64 = authHeaders.split('Basic ')[1];
   if(!base64)
-    return errorHandler(new Error('Authorization failed. Username and Password required.'), res)
+    return errorHandler(new Error('Authorization failed. Username and Password required.'), res);
 
-  let [username, password] = Buffer.from(base64, 'base64').toString().split(':')
-  req.auth = {username, password}
+  let [username, password] = Buffer.from(base64, 'base64').toString().split(':');
+  req.auth = {username, password};
 
   if(!req.auth.username)
-    return errorHandler(new Error('Authorization failed. Username required.'), res)
+    return errorHandler(new Error('Authorization failed. Username required.'), res);
   if(!req.auth.password)
-    return errorHandler(new Error('Authorization failed. Password required.'), res)
+    return errorHandler(new Error('Authorization failed. Password required.'), res);
 
-  next()
-}
+  next();
+};
